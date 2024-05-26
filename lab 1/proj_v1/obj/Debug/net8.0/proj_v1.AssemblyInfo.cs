60f1a21fa5d124a50dc21c4f23752ff64c0cc538
Bohdan Pasichnyk <vt221_pbr@student.ztu.edu.ua>
2024-05-26 19:39:20 +0100

    оновлення шляху

--- lab 1/proj_v1/obj/Debug/net8.0/proj_v1.AssemblyInfo.cs
+++ lab 1/proj_v1/obj/Debug/net8.0/proj_v1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proj_v1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c8f7e920dd18b01abb8c4879636c88215228a0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("proj_v1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proj_v1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
