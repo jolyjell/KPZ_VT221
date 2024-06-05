2541cf1340843d600dfb37a790602c14624c40c6
Bohdan Pasichnyk <vt221_pbr@student.ztu.edu.ua>
2024-06-05 20:05:01 +0100

    task2

--- lab-2/Task2/Program.cs
+++ lab-2/Task2/Program.cs
@@ -1,2 +1,135 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace AbstractFactoryPattern
+{
+    public abstract class Laptop
+    {
+        public abstract void ShowDetails();
+    }
+
+    public abstract class Smartphone
+    {
+        public abstract void ShowDetails();
+    }
+
+    public class IProneLaptop : Laptop
+    {
+        public override void ShowDetails()
+        {
+            Console.WriteLine("IProne Laptop");
+        }
+    }
+
+    public class IProneSmartphone : Smartphone
+    {
+        public override void ShowDetails()
+        {
+            Console.WriteLine("IProne Smartphone");
+        }
+    }
+
+    public class KiaomiLaptop : Laptop
+    {
+        public override void ShowDetails()
+        {
+            Console.WriteLine("Kiaomi Laptop");
+        }
+    }
+
+    public class KiaomiSmartphone : Smartphone
+    {
+        public override void ShowDetails()
+        {
+            Console.WriteLine("Kiaomi Smartphone");
+        }
+    }
+
+    public class BalaxyLaptop : Laptop
+    {
+        public override void ShowDetails()
+        {
+            Console.WriteLine("Balaxy Laptop");
+        }
+    }
+
+    public class BalaxySmartphone : Smartphone
+    {
+        public override void ShowDetails()
+        {
+            Console.WriteLine("Balaxy Smartphone");
+        }
+    }
+
+    public abstract class TechFactory
+    {
+        public abstract Laptop CreateLaptop();
+        public abstract Smartphone CreateSmartphone();
+    }
+
+    public class IProneFactory : TechFactory
+    {
+        public override Laptop CreateLaptop()
+        {
+            return new IProneLaptop();
+        }
+
+        public override Smartphone CreateSmartphone()
+        {
+            return new IProneSmartphone();
+        }
+    }
+
+    public class KiaomiFactory : TechFactory
+    {
+        public override Laptop CreateLaptop()
+        {
+            return new KiaomiLaptop();
+        }
+
+        public override Smartphone CreateSmartphone()
+        {
+            return new KiaomiSmartphone();
+        }
+    }
+
+    public class BalaxyFactory : TechFactory
+    {
+        public override Laptop CreateLaptop()
+        {
+            return new BalaxyLaptop();
+        }
+
+        public override Smartphone CreateSmartphone()
+        {
+            return new BalaxySmartphone();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TechFactory iproneFactory = new IProneFactory();
+            TechFactory kiaomiFactory = new KiaomiFactory();
+            TechFactory balaxyFactory = new BalaxyFactory();
+
+            Laptop iproneLaptop = iproneFactory.CreateLaptop();
+            Smartphone iproneSmartphone = iproneFactory.CreateSmartphone();
+
+            Laptop kiaomiLaptop = kiaomiFactory.CreateLaptop();
+            Smartphone kiaomiSmartphone = kiaomiFactory.CreateSmartphone();
+
+            Laptop balaxyLaptop = balaxyFactory.CreateLaptop();
+            Smartphone balaxySmartphone = balaxyFactory.CreateSmartphone();
+
+            iproneLaptop.ShowDetails();
+            iproneSmartphone.ShowDetails();
+
+            kiaomiLaptop.ShowDetails();
+            kiaomiSmartphone.ShowDetails();
+
+            balaxyLaptop.ShowDetails();
+            balaxySmartphone.ShowDetails();
+        }
+    }
+}
--- lab-2/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ lab-2/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2c0120e49521fe559f4566ef45242d3b315745e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
